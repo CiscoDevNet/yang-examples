e2ac0775c2f5297b4195a38c48dc4f192dd6c057
darien-hirotsu <darien@sdnessentials.com>
2016-02-24 10:48:09 -0800

    Adding all YANG content

---
+++ README.md
@@ -0,0 +1,4 @@
+# yang-examples
+Sample code for the Cisco DevNet YANG Learning Labs
+
+You can find step-by-step tutorials that walk through this sample code on [Cisco DevNet](http://learninglabs.cisco.com).
--- yang-102/get_ietf_interfaces_yang.py
+++ yang-102/get_ietf_interfaces_yang.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+import logging
+from lxml import etree
 from ncclient import manager
+from ncclient.xml_ import *
 import sys
 
-# the variables below assume the user is requesting access
-# to a IOS-XE device running in the DevNet Always On SandBox
-# use the IP address or hostname of your IOS-XE device
+
+# the variables below assume the user is requesting
+# access to a CSR1000V device running in the DevNet
+# Always On SandBox
+# use the IP address or hostname of your CSR1000V device
 HOST = '172.16.126.250'
-# use the NETCONF port for your IOS-XE device
+# use the NETCONF port for your CSR1000V device
 PORT = 2022
-# use the user credentials for your IOS-XE device
+# use the user credentials for your CSR1000V device
 USER = 'cisco'
 PASS = 'cisco'
 
@@ -21,10 +26,11 @@ def main():
                          hostkey_verify=False, device_params={'name': 'default'},
                          look_for_keys=False, allow_agent=False) as m:
 
-        # print all NETCONF capabilities
-        print('***Here are the Remote Devices Capabilities***')
-        for capability in m.server_capabilities:
-            print(capability)
+        # print YANG module
+        print('***Here is the YANG Module***')
+        data = m.get_schema('ietf-interfaces')
+        print(data.xml)
+
 
 if __name__ == '__main__':
     sys.exit(main())
